b5435e8e6e9e93849f42a7413b7c16f016a251ab
Andre Lima <andresdslima@gmail.com>
2022-08-18 13:52:16 +0200

    fixing bugs

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,3 @@
 export const environment = {
   production: false,
-  weatherApiBaseUrl: 'https://visual-crossing-weather.p.rapidapi.com/forecast',
-  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
-  XRapidAPIHostHeaderValue: 'visual-crossing-weather.p.rapidapi.com',
-  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
-  XRapidAPIKeyHeaderValue: '5386461ca6mshe69b4439791582fp1f79a9jsn1d9908b80f31',
 };
